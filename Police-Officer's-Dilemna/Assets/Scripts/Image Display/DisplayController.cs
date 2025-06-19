a7e2d0b0e27fac3ba302926c7b6cf37d76dc0901
cbb1997 <cbd45068@gmail.com>
2025-06-19 12:58:37 -0700

    Finished Basic Background Generation

--- Police-Officer's-Dilemna/Assets/Scripts/Image Display/DisplayController.cs
+++ Police-Officer's-Dilemna/Assets/Scripts/Image Display/DisplayController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System;
 
 public class DisplayController : MonoBehaviour
 {
@@ -8,13 +10,50 @@ public class DisplayController : MonoBehaviour
 
     [SerializeField] private GameObject[] m_People, m_BG;
 
+    private float m_CurrentGenerationTime;
+    private GameObject m_CurrentPerson, m_CurrentBG;
+
     private void Start()
     {
-        
+        if (m_DisplayData == null)
+        {
+            Debug.LogError("Missing DisplayData. Terminating Application.");
+            QuitGame();
+        }
+
+        GenerateImage();
     }
     
     private void Update()
     {
         
+    }
+
+    private void GenerateImage()
+    {
+        m_CurrentGenerationTime = UnityEngine.Random.Range(m_DisplayData.MinImageTime, m_DisplayData.MaxImageTime);
+
+        StartCoroutine(GenerateHelper(m_CurrentGenerationTime));
+    }
+
+    private IEnumerator GenerateHelper(float seconds)
+    {
+        m_CurrentBG = Instantiate(m_BG[new System.Random().Next(0, m_BG.Length)]);
+
+        yield return new WaitForSeconds(seconds);
+
+        Destroy(m_CurrentBG);
+
+        GenerateImage();
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false; 
+            return;
+#endif
+
+        Application.Quit();
     }
 }
