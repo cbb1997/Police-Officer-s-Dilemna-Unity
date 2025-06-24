2b32b0bc1f1062ab1b902acfbea006da825dc00a
cbb1997 <cbd45068@gmail.com>
2025-06-24 16:10:45 -0700

    More Model Database Setup

--- Police-Officer's-Dilemna/Assets/Scripts/Image Display/BGData.cs
+++ Police-Officer's-Dilemna/Assets/Scripts/Image Display/BGData.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "BGData", menuName = "ScriptableObjects/BGData")]
 public class BGData : ImageData
 {
-
+    // private var Value;
 
     protected override void Awake()
     {
