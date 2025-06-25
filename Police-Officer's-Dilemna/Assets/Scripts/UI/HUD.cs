8572b2d2fd6a260356c7edba27fd128cfad9b42d
cbb1997 <cbd45068@gmail.com>
2025-06-24 17:32:16 -0700

    HUD Updates

--- Police-Officer's-Dilemna/Assets/Scripts/UI/HUD.cs
+++ Police-Officer's-Dilemna/Assets/Scripts/UI/HUD.cs
@@ -2,18 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HUD : MonoBehaviour
 {
-    
+    [SerializeField] private TMP_Text m_ScoreUI;
+
+    [SerializeField] private Button m_ShootButton, m_ClearButton;
+
+    private string m_BaseScoreText;
+
     private void Start()
     {
-        
-    }
+        SetButtonEnabled(false);
 
+        m_BaseScoreText = m_ScoreUI.text;
+
+        DataCollector.OnScoreChanged += UpdateScoreText;
+
+        DisplayController.OnPersonGenerated += (imageData) => SetButtonEnabled(true);
+    }
 
     private void UpdateScoreText(int score)
     {
+        m_ScoreUI.text = $"{m_BaseScoreText} {score}";
 
+        SetButtonEnabled(false);
+    }
+
+    private void SetButtonEnabled(bool enabled)
+    {
+        m_ShootButton.interactable = enabled;
+        m_ClearButton.interactable = enabled;
     }
 }
