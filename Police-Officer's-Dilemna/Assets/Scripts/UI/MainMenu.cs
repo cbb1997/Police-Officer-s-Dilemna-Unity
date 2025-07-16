9e92e5e1143ef14ac1147f6321886a329ee0189d
cbb1997 <cbd45068@gmail.com>
2025-07-16 16:58:02 -0700

    Main Menu Updates

--- Police-Officer's-Dilemna/Assets/Scripts/UI/MainMenu.cs
+++ Police-Officer's-Dilemna/Assets/Scripts/UI/MainMenu.cs
@@ -7,24 +7,56 @@ using System;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button m_StartButton;
+    [SerializeField] private Button m_StartButton, m_NextButton;
+    [SerializeField] private TMP_Dropdown m_HandDropdown;
+
+    [SerializeField] private GameObject[] m_Instructions;
 
     [SerializeField] private GameObject m_TrialObjects;
 
     private int m_DominantHand = 1;
+
+    private int m_CurrentScreen;
 
     public static Action<int> OnGameStart;
 
     private void Start()
     {
         m_StartButton.onClick.AddListener(StartGame);
+        m_StartButton.onClick.AddListener(Next);
 
-        FindObjectOfType<TMP_Dropdown>().onValueChanged.AddListener(SetDominantHand);
+        m_HandDropdown.onValueChanged.AddListener(SetDominantHand);
+    }
+
+    private void OnDestroy()
+    {
+        m_StartButton.onClick.RemoveListener(StartGame);
+        m_StartButton.onClick.RemoveListener(Next);
+
+        m_HandDropdown.onValueChanged.RemoveListener(SetDominantHand);
     }
 
     public void SetDominantHand(int hand)
     {
         m_DominantHand = hand + 1;
+    }
+
+    private void Next()
+    {
+        m_HandDropdown.gameObject.SetActive(false);
+
+        m_CurrentScreen++;
+
+        if (m_CurrentScreen >= m_Instructions.Length - 1)
+        {
+            m_NextButton.gameObject.SetActive(false);
+            m_StartButton.gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < m_Instructions.Length; i++)
+        {
+            m_Instructions[i].SetActive(i == m_CurrentScreen);
+        }
     }
 
     private void StartGame()
